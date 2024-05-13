0d6ec32e6ac1ae6e73bca2e49b1ef8d4329f63b6
jinyoung <gjwlsdud730@gmail.com>
2024-05-13 19:58:55 +0900

    update src

--- src/test/java/hello/board/BoardApplicationTests.java
+++ src/test/java/hello/board/BoardApplicationTests.java
@@ -2,8 +2,10 @@ package hello.board;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("tests")
 class BoardApplicationTests {
 
 	@Test
